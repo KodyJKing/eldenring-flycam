6cdfa7320d8a7b661095e077a16fa1eebad00bba
KJK <kody.j.king@gmail.com>
2022-11-19 20:27:57 -0800

    Stripped out unused code.

--- lua/addresses.lua
+++
@@ -1,9 +0,0 @@
-return {
-    chrCam = '["eldenring.exe"+03C482C8]+60',
-    chrCamVTable = '"eldenring.exe"+29776F8',
-
-    playerIns = '["eldenring.exe"+03A18D80]',
-    playerIns_chrDataModule = '[[["eldenring.exe"+03A18D80]]+190]+0',
-    playerIns_chrPhysicsModule = '[[["eldenring.exe"+03A18D80]]+190]+68',
-    playerIns_hp = '[[[["eldenring.exe"+03A18D80]]+190]+0]+138',
-}
